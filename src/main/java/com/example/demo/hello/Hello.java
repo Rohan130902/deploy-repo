fc30f3b850b244b306fcd642a4aba61bc7211d00
Rohan130902 <91385161+Rohan130902@users.noreply.github.com>
2025-11-27 00:43:14 +0530

    Update Hello.java

--- src/main/java/com/example/demo/hello/Hello.java
+++ src/main/java/com/example/demo/hello/Hello.java
@@ -3,9 +3,6 @@ package com.example.demo.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
 @RestController
 public class Hello {
  @GetMapping("/hello")
