a3735ff9786a5f169686cad1a38261a3559aa2de
Rohan130902 <91385161+Rohan130902@users.noreply.github.com>
2025-12-19 23:51:02 +0530

    Update NeonRequest.java

--- src/main/java/com/example/demo/hello/NeonRequest.java
+++ src/main/java/com/example/demo/hello/NeonRequest.java
@@ -8,6 +8,8 @@ public class NeonRequest {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+
+    
     public Double getValue() { return value; }
     public void setValue(Double value) { this.value = value; }
 }
